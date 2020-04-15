9396cf70aa2cde079a2b8b61422a23a942e0ad22
winny7 <45691074+winny7@users.noreply.github.com>
2020-04-14 21:52:01 -0700

    Add files via upload

---
+++ ArrayOperationsTester.java
@@ -0,0 +1,60 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArrayOperationsTester {
+
+    @Test
+    public void testMaxElement() {
+        int[] array = {3, 2, 1};
+        int maxExpected = 3;
+        int maxActual = ArrayOperations.maxElement(array);
+        assertEquals(maxExpected, maxActual);
+    }
+    
+    @Test
+    public void testMinElement() {
+        // TODO: Fill in this method for ArrayOperationsTester
+    }
+    
+    @Test
+    public void testMean() {
+        // TODO: Fill in this method for ArrayOperationsTester
+    }
+    
+    @Test
+    public void testMedian() {
+        // TODO: Fill in this method for ArrayOperationsTester
+    }
+    
+    @Test
+    public void testMode() {
+        // TODO: Fill in this method for ArrayOperationsTester
+    }
+    
+    @Test
+    public void testSearchArray() {
+        // TODO: Fill in this method for ArrayOperationsTester
+    }
+    
+    @Test
+    public void testSortArray() {
+        // TODO: Fill in this method for ArrayOperationsTester
+    }
+    
+    @Test
+    public void testReverseArray() {
+        // TODO: Fill in this method for ArrayOperationsTester
+    }
+    
+    @Test
+    public void testIsEqual() {
+        // TODO: Fill in this method for ArrayOperationsTester
+    }
+    
+    @Test
+    public void testDeepCopyArray() {
+        // TODO: Fill in this method for ArrayOperationsTester
+    }
+    
+}
---
+++ GradeReport.java
@@ -0,0 +1,29 @@
+public class GradeReport {
+    
+    // Sample Constants
+    public static final int NUM_RQ = 16;
+    public static final int NUM_PA = 8;
+    public static final int NUM_EXAM = 8;  
+    
+    /** 
+     * Main method that reads the input file and outputs the 
+     * corresponding letter grade into an output file
+     * @param  arrgs[0] - input file
+     * @param  arrgs[1] - output file
+     * @return void
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        
+
+    }
+    
+
+    public static double computeAverage(double total, int num, int maxMarks) {
+
+    }
+
+
+    public static String computeLetterGrades(double totalPoints) {
+
+    }
+}
